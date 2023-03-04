1d77d6adc28ac66f90afc322cd524310afb5156f
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-04 14:29:32 -0300

    test: DbSaveMatch should throw if GetIdByTeamNameRepository throws

--- src/data/usecase/db.save.match.ts
+++ src/data/usecase/db.save.match.ts
@@ -1,13 +1,25 @@
 import { Match } from "@/domain/model"
 import { SaveMatch } from "@/domain/usecase"
-import { SaveMatchRepository } from "../protocol"
+import { GetIdByTeamNameRepository, SaveMatchRepository } from "../protocol"
 
 export class DbSaveMatch implements SaveMatch {
 	constructor (
+        private readonly getIdByTeamNameRepository: GetIdByTeamNameRepository, 
         private readonly saveMatchRepository: SaveMatchRepository
 	) {}
 
 	async save (data: Match): Promise<void> {
-		await this.saveMatchRepository.save(data)
+		const firstTeam = await this.getIdByTeamNameRepository.get(data.teams[0])
+		const secondTeam = await this.getIdByTeamNameRepository.get(data.teams[1])
+		const saveMatchRepositoryParams: SaveMatchRepository.Params = {
+			title: data.title,
+			date: data.date,
+			teams: [
+				firstTeam,
+				secondTeam,
+			]
+			
+		}
+		await this.saveMatchRepository.save(saveMatchRepositoryParams)
 	}
 }
--- test/data/usecase/db.save.match.spec.ts
+++ test/data/usecase/db.save.match.spec.ts
@@ -1,12 +1,24 @@
 import { DbSaveMatch } from "@/data/usecase"
-import { SaveMatchRepositorySpy } from "../protocol"
+import { GetIdByTeamNameRepositorySpy, SaveMatchRepositorySpy } from "../protocol"
 
 import { mockMatch, throwError } from "test/domain/mock"
 
 describe("DbSaveMatch", () => {
-	test("should throw if SaveMatchRepositorySpy throws", async () => {
+	test("should throw if GetIdByTeamNameRepository throws", async () => {
+		const getIdByTeamNameRepositorySpy = new GetIdByTeamNameRepositorySpy()
 		const saveMatchRepositorySpy = new SaveMatchRepositorySpy()
-		const sut = new DbSaveMatch(saveMatchRepositorySpy)
+		const sut = new DbSaveMatch(getIdByTeamNameRepositorySpy, saveMatchRepositorySpy)
+		jest.spyOn(getIdByTeamNameRepositorySpy, "get").mockImplementationOnce(throwError)
+    
+		const promise = sut.save(mockMatch())
+    
+		await expect(promise).rejects.toThrowError()
+	})
+
+	test("should throw if SaveMatchRepositorySpy throws", async () => {
+		const getIdByTeamNameRepositorySpy = new GetIdByTeamNameRepositorySpy()
+		const saveMatchRepositorySpy = new SaveMatchRepositorySpy()
+		const sut = new DbSaveMatch(getIdByTeamNameRepositorySpy, saveMatchRepositorySpy)
 		jest.spyOn(saveMatchRepositorySpy, "save").mockImplementationOnce(throwError)
     
 		const promise = sut.save(mockMatch())
