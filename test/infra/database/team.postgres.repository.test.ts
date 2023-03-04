864d3616419f2f83613e88ebc79c33f4f95386f6
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-04 16:18:28 -0300

    test: TeamPostgresRepository should get correct team

---
+++ test/infra/database/team.postgres.repository.test.ts
@@ -0,0 +1,46 @@
+import { faker } from "@faker-js/faker"
+
+import { GetTeamByNameRepository } from "@/data/protocol"
+import { prisma } from "@/infra/database"
+
+class TeamPostgresRepository implements GetTeamByNameRepository {
+	async getByName (name: string): Promise<GetTeamByNameRepository.Result> {
+		const team = await prisma.team.findFirst({
+			where: {
+				title: name
+			},
+			include: {
+				matchs: true
+			}
+		})
+		if (team) {
+			return {
+				id: team.id,
+				title: team.title,
+			}
+		}
+	}
+}
+
+describe("TeamPostgresRepository", () => {
+	afterEach(async () => {
+		await prisma.team.deleteMany({})
+	})
+
+	test("should get correct team", async () => {
+		const sut = new TeamPostgresRepository()
+		const fakeTeamName = faker.random.words()
+		await prisma.team.create({
+			data: {
+				title: fakeTeamName
+			}
+		})
+
+		const team = await sut.getByName(fakeTeamName)
+
+		expect(team).toStrictEqual({
+			id: 1,
+			title: fakeTeamName,
+		})
+	})
+})
