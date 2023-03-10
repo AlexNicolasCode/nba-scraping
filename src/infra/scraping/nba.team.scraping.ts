c84dfde71bbeb2e03e330c48bad485bbb286b497
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-10 15:22:45 -0300

    feat: NBATeamCheerioScraping was created

---
+++ src/infra/scraping/nba.team.scraping.ts
@@ -0,0 +1,28 @@
+import { TeamScraping } from "@/data/protocol"
+import * as cheerio from "cheerio"
+
+export class NBATeamCheerioScraping implements TeamScraping {
+	async run (pageContent: string): Promise<TeamScraping.Result> {
+		const $page = cheerio.load(pageContent)
+		const teamsLinks = $page(".AnchorLink:contains('Estatísticas')")
+			.map((index: number, team: cheerio.Element) => {
+				return $page(team).attr("href")
+			}).get()
+		const teamAcronym = $page(".AnchorLink:contains('Estatísticas')")
+			.map((index: number, team: cheerio.Element) => {
+				const link = $page(team).attr("href")
+				const acronymPositionInString = 6 
+				if (link) {
+					return link.split("/")[acronymPositionInString]
+				}
+			}).get()
+		const teams = $page("h2").map((index: number, team: cheerio.Element) => {
+			return {
+				name: $page(team).text(),
+				acronym: teamAcronym[index],
+				link: teamsLinks[index],
+			}
+		}).get()
+		return teams
+	}
+}
--- test/infra/scraping/nba.team.scraping.test.ts
+++ test/infra/scraping/nba.team.scraping.test.ts
@@ -1,33 +1,8 @@
-import { TeamScraping } from "@/data/protocol"
-
 import * as cheerio from "cheerio"
-import { throwError } from "test/domain/mock"
 
-class NBATeamCheerioScraping implements TeamScraping {
-	async run (pageContent: string): Promise<TeamScraping.Result> {
-		const $page = cheerio.load(pageContent)
-		const teamsLinks = $page(".AnchorLink:contains('Estatísticas')")
-			.map((index: number, team: cheerio.Element) => {
-				return $page(team).attr("href")
-			}).get()
-		const teamAcronym = $page(".AnchorLink:contains('Estatísticas')")
-			.map((index: number, team: cheerio.Element) => {
-				const link = $page(team).attr("href")
-				const acronymPositionInString = 6 
-				if (link) {
-					return link.split("/")[acronymPositionInString]
-				}
-			}).get()
-		const teams = $page("h2").map((index: number, team: cheerio.Element) => {
-			return {
-				name: $page(team).text(),
-				acronym: teamAcronym[index],
-				link: teamsLinks[index],
-			}
-		}).get()
-		return teams
-	}
-}
+import { NBATeamCheerioScraping } from "@/infra/scraping"
+
+import { throwError } from "test/domain/mock"
 
 describe("NBATeamCheerioScraping", () => {
 	test("should throw when cheerio throws", async () => {
