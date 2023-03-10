9660782d74bac1404c7a2748796e56eaf6b89adc
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-10 19:48:53 -0300

    test: PuppeteerClientRepository should throw if puppeteer throws

---
+++ test/infra/http/puppeteer.client.repository.test.ts
@@ -0,0 +1,26 @@
+import puppeteer from "puppeteer"
+
+import { GetPageDataRepository } from "@/data/protocol/http"
+import { throwError } from "test/domain/mock"
+
+export class PuppeteerClientRepository implements GetPageDataRepository {
+	async getPageData (link: string): Promise<GetPageDataRepository.Result> {
+		const browser = await puppeteer.launch()
+		const pagePuppeteer = await browser.newPage()
+		await pagePuppeteer.goto(link)
+		const content = await pagePuppeteer.content()
+		await browser.close()
+		return { data: content }
+	}
+}
+
+describe("PuppeteerClientRepository", () => {
+	test("should throw if puppeteer throws", async () => {
+		const sut = new PuppeteerClientRepository()
+		jest.spyOn(puppeteer, "launch").mockImplementationOnce(throwError)
+
+		const promise = sut.getPageData("https://example.com")
+        
+		await expect(promise).rejects.toThrow()
+	})
+})
