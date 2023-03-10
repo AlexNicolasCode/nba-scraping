29b2a67410e3fd1b3ba8339562c6818c384e1273
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-10 20:48:20 -0300

    test: ClientGetPage should throw when GetPageRepository throws

---
+++ test/data/usecase/client.get.page.spec.ts
@@ -0,0 +1,27 @@
+import { GetPage } from "@/domain/usecase"
+import { GetPageDataRepository } from "@/data/protocol"
+
+import { GetPageDataRepositorySpy } from "test/data/protocol"
+import { throwError } from "test/domain/mock"
+
+export class ClientGetPage implements GetPage {
+	constructor (
+        private readonly getPageRepository: GetPageDataRepository 
+	) {}
+
+	async getPage (link: string): Promise<GetPage.Result> {
+		return await this.getPageRepository.getPageData(link)
+	}
+}
+
+describe("ClientGetPage", () => {
+	test("should throw when GetPageRepository throws", async () => {
+		const getPageDataRepositorySpy = new GetPageDataRepositorySpy()
+		const sut = new ClientGetPage(getPageDataRepositorySpy)
+		jest.spyOn(getPageDataRepositorySpy, "getPageData").mockImplementationOnce(throwError)
+
+		const promise = sut.getPage("any_link")
+
+		await expect(promise).rejects.toThrow()
+	})
+})
