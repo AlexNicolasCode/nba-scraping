f7e2f6b3c89133fe12f40019a75088a50ffec981
Alex Nicolas <alexoliveira604@gmail.com>
2023-03-10 21:10:50 -0300

    test: NbaTeamsScraping should throw when TeamScraping throws

--- test/scraping/nba.teams.scraping.spec.ts
+++ test/scraping/nba.teams.scraping.spec.ts
@@ -1,25 +1,44 @@
+import { TeamScraping } from "@/data/protocol"
 import { GetPage } from "@/domain/usecase"
 import { Scraping } from "@/scraping/protocol"
+import { TeamScrapingSpy } from "test/data/protocol"
 import { GetPageSpy, throwError } from "test/domain/mock"
 
 class NbaTeamsScraping implements Scraping {
 	constructor (
-        private readonly getPage: GetPage
+        private readonly getPage: GetPage,
+        private readonly teamScraping: TeamScraping,
 	) {}
 
 	async run (link: string): Promise<void> {
 		const page = await this.getPage.getPage(link)
+		if (!page) {
+			throw Error("page not found")
+		}
+		const teams = await this.teamScraping.run(page.data)
 	}
 }
 
 describe("NbaTeamsScraping", () => {
 	test("should throw when GetPage throws", async () => {
+		const teamScrapingSpy = new TeamScrapingSpy()
 		const getPageSpy = new GetPageSpy()
-		const sut = new NbaTeamsScraping(getPageSpy)
+		const sut = new NbaTeamsScraping(getPageSpy, teamScrapingSpy)
 		jest.spyOn(getPageSpy, "getPage").mockImplementationOnce(throwError)
 
 		const promise = sut.run("any_link")
         
 		await expect(promise).rejects.toThrow()
+	})
+
+	test("should throw when TeamScraping throws", async () => {
+		const teamScrapingSpy = new TeamScrapingSpy()
+		const getPageSpy = new GetPageSpy()
+		const sut = new NbaTeamsScraping(getPageSpy, teamScrapingSpy)
+		jest.spyOn(teamScrapingSpy, "run").mockImplementationOnce(throwError)
+
+		const promise = sut.run("any_link")
+        
+		await expect(promise).toBe(getPageSpy.result)
 	})
 })
